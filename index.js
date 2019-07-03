5fb74a475311cc2a2e8f55e739f65968a86321b3
Petre Leonard Macamete <petreleonartdos@gmail.com>
2019-07-03 13:10:16 +0300

    dynamically adding categories and write object on file

--- index.js
+++ index.js
@@ -1,59 +1,26 @@
+var fs  = require('fs');
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('checkouts-by-title.csv')
+  input: fs.createReadStream('checkouts-by-title.csv')
 });
+var util = require('util');
+var fs  = require('fs');
 
-var categoryCount ={'\"ER': 0,
-'"FLASHCARD': 0,
-'\"PICTURE': 0,
-'"REGPRINT': 0,
-'"SLIDE': 0,
-'"SOUNDCASS': 0,
-'"SOUNDDISC': 0,
-'"VIDEOCASS': 0,
-ATLAS: 0,
-AUDIOBOOK: 0,
-BOOK: 0,
-COMIC: 0,
-COMPFILE: 0,
-CR: 0,
-EBOOK: 0,
-ER: 0,
-GLOBE: 0,
-KIT: 0,
-LARGEPRINT: 0,
-MAGAZINE: 0,
-MAP: 0,
-MaterialType: 0,
-MICROFORM: 0,
-MIXED: 0,
-MOVIE: 0,
-MUSIC: 0,
-MUSICSNDREC: 0,
-NOTATEDMUSIC: 0,
-PICTURE: 0,
-REGPRINT: 0,
-REMOTESEN: 0,
-SLIDE: 0,
-SONG: 0,
-SOUNDCASS: 0,
-SOUNDDISC: 0,
-SOUNDREC: 0,
-TELEVISION: 0,
-UNSPECIFIED: 0,
-VIDEO: 0,
-VIDEOCART: 0,
-VIDEOCASS: 0,
-VIDEODISC: 0,
-VIDEOREC: 0,
-VISUAL: 0};
+var categoryCount ={};
 
 
 lineReader.on('line', function (line) {
   var row= line.split(',');
-  categoryCount[row[2]]+=1;
+  var category = row[2];
+  category = category.replace(/('|")/g, "");
+  if(typeof categoryCount[category]=='undefined'){
+    
+    categoryCount[category]=0;
+  }
+  categoryCount[category]+=1;
 });
 
 lineReader.on('close', function () {
   console.log(categoryCount);
+  fs.writeFileSync('./data.json', util.inspect(categoryCount) , 'utf-8');
 });
 
