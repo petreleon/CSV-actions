db0a246ef379f11466c7c830b08710983a84ea97
Petre Leonard Macamete <petreleonartdos@gmail.com>
2019-07-03 13:47:05 +0300

    write asynchronous

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs  = require('fs');
 var lineReader = require('readline').createInterface({
   input: fs.createReadStream('checkouts-by-title.csv')
+  //input: fs.createReadStream('test.csv')
 });
 var util = require('util');
 var fs  = require('fs');
-
+let t0 = new Date().getTime();
 var categoryCount ={};
 
 
@@ -21,6 +22,11 @@ lineReader.on('line', function (line) {
 
 lineReader.on('close', function () {
   console.log(categoryCount);
-  fs.writeFileSync('./data.json', util.inspect(categoryCount) , 'utf-8');
+  fs.writeFile('./data.json', util.inspect(categoryCount), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  let t1 = new Date().getTime();
+  console.log(t1-t0);
 });
 
